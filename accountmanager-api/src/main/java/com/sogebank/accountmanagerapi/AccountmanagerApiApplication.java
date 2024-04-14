6e9c8add72a776ab2e9953f8e960dd45053e07db
maxloversist19@gmail.com <maxloversist19@gmail.com>
2024-04-14 11:49:35 -0300

    Inicial backent commit

---
+++ accountmanager-api/src/main/java/com/sogebank/accountmanagerapi/AccountmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.sogebank.accountmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountmanagerApiApplication.class, args);
+	}
+
+}
---
+++ accountmanager-api/src/test/java/com/sogebank/accountmanagerapi/AccountmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogebank.accountmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
